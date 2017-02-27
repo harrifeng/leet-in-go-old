2916f065890001566ab26fd2305d1d4a8fff211b
Haoran Feng <haoran.feng@alo7.com>
2017-02-27 10:56:02 +0800

    reorg

--- 005_longest_palindromic_substring.go
+++ 005_longest_palindromic_substring.go
@@ -9,15 +9,14 @@ func longestPalindrome(s string) string {
 	ns := "^#" + strings.Join(strings.Split(s, ""), "#") + "#$"
 	cnt := [3000]int{}
 
-	tmpi_c := 0
-	_ = tmpi_c
 	tmpi := 0
-	maxi := 0
+	tmpiCenter := 0
 	maxv := 0
+	maxvIndex := 0
 
 	for i := 1; i < len(ns)-1; i++ {
 		if i < tmpi {
-			cnt[i] = int(math.Min(float64(cnt[2*tmpi_c-i]),
+			cnt[i] = int(math.Min(float64(cnt[2*tmpiCenter-i]),
 				float64(tmpi-i)))
 		} else {
 			cnt[i] = 1
@@ -29,14 +28,14 @@ func longestPalindrome(s string) string {
 
 		if i+cnt[i] > tmpi {
 			tmpi = i + cnt[i]
-			tmpi_c = i
+			tmpiCenter = i
 		}
 
 		if maxv < cnt[i] {
 			maxv = cnt[i]
-			maxi = i
+			maxvIndex = i
 		}
 	}
 
-	return s[maxi/2-maxv/2 : maxi/2-maxv/2+maxv-1]
+	return s[maxvIndex/2-maxv/2 : maxvIndex/2-maxv/2+maxv-1]
 }
